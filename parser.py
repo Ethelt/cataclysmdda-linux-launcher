eeef08809286337c4c5fdf3eda0e7e7e02839be5
Ethelt <42093730+Ethelt@users.noreply.github.com>
2020-05-02 11:09:07 +0200

    Fixed a bug with parser reading non-existent variable
    For some reason, the file I was running while testing was different than the one I pushed on Github, which is now fixed.

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 import json
 
-releases = json.loads(r.content)
+with open("json") as releases_info:
+    releases = json.load(releases_info)
 for release in releases[0]['assets']:
     if release["label"] == "Linux_x64 Tiles":
         print(release["browser_download_url"])
