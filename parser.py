0e1c86739bce4b77a6c9a7eb8283160bae0131ba
Ethelt <42093730+Ethelt@users.noreply.github.com>
2020-05-11 11:44:41 +0200

    Fixed a crash when newest version doesn't have a Linux Tiles build.

--- parser.py
+++ parser.py
@@ -2,6 +2,10 @@ import json
 
 with open("json") as releases_info:
     releases = json.load(releases_info)
-for release in releases[0]['assets']:
-    if release["label"] == "Linux_x64 Tiles":
-        print(release["browser_download_url"])
+i = 0
+while True:
+    for release in releases[i]['assets']:
+        if release["label"] == "Linux_x64 Tiles":
+            print(release["browser_download_url"])
+            exit() 
+    i += 1
